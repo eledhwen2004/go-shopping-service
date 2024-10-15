bfc48d413fc8fc52a30af1998a8b1af6b5bdca3c
emirhanuzun <emirhanuzun98123@gmail.com>
2024-10-15 21:10:13 +0300

    feat:some changes

--- api/user.go
+++ api/user.go
@@ -1,1 +1,15 @@
+package api
 
+type UserUpdatePayload struct {
+	ID          string
+	Name        string `validate:"omitempty,required"`
+	Email       string `validate:"omitempty,required,email"`
+	Password    string `validate:"omitempty,required"`
+	PhoneNumber string `validate:"omitempty,required"`
+}
+
+type UserProfile struct {
+	ID    string
+	Name  string
+	Email string
+}
